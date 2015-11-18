0528b116b7ecd5a718a684ea564c8fecc62a29fc
Christoph Mueller <chrism@g-graphix.de>
2015-11-18 16:53:03 +0100

    Completely ignoring mscorlib and still working

--- App/Program.cs
+++ App/Program.cs
@@ -1,13 +1,33 @@
 ﻿using System;
+using System.Reflection;
 using DaLibrary;
+using static System.Console;
 
 namespace App
 {
+    class ReflectMe
+    {
+        public int I;
+        public string Str;
+    };
+
     class Program
     {
+        static void ReflectIt(Type o)
+        {
+            WriteLine(o.Name + " contains:");
+            foreach (MemberInfo m in o.GetMembers())
+            {
+                WriteLine(m.Name);
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello JSIL");
+            WriteLine("Hello JSIL");
+
+            ReflectIt(new ReflectMe().GetType());
+
             DaClass daClass = new DaClass { I = 42, OutputFunc = (s) => { Console.WriteLine(s);}};
             daClass.DoSometing();
         }
